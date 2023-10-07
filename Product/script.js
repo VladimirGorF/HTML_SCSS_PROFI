6c46fad1221b4483095d47c3ad5a50d360a69b88
Vladimir Gorbunov <110591063+VladimirGorF@users.noreply.github.com>
2023-10-07 12:15:50 +0400

    Добавил корректное наведение в fetured items

--- Product/script.js
+++ Product/script.js
@@ -6,7 +6,17 @@ fetch("data.json")
     const cardsDiv = document.querySelector(".feture_box");
     data.forEach(({ image, card_name, description, price }) => {
       const productElem = ` 
-      <a href="product.html" class="feture_items"> <img src=${image} alt="image item" class="fet_image">
+      <a href="product.html" class="feture_items">
+
+        <div class="item2_img_box">
+          <img src=${image} alt="image item" class="fet_image">
+            <div class="image_filter"></div>
+            <div class="fet_item2_img2_inform">              
+              <img src="img/Basket2.svg" alt="Basket2">                  
+              <div class="add">Add to Cart</div>                  
+            </div>                          
+        </div>
+
         <div class="fet_item_inform">
           <div class="fet_title">${card_name}</div>
           <div class="fet_text">${description}</div>
